3559852eb4037172c957f78503e7fec969e2636f
Foo Fang Woei <fangwoei22f@forward.edu.my>
2023-10-04 11:48:56 +0800

    ...

--- src/api/data.js
+++ src/api/data.js
@@ -1,5 +1,4 @@
-// export const API_URL =
-//   process.env.NODE_ENV === "development"
-//     ? "http://localhost:1226"
-//     : "http://10.1.104.9:1226";
-export const API_URL = "http://localhost:1226";
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:1226"
+    : "http://10.1.104.9:1226";
